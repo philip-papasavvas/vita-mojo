35ca24b94f4ba509243e9a21937d2a7e450a5ac9
philip-papasavvas <39246705+philip-papasavvas@users.noreply.github.com>
2024-08-19 19:53:37 +0100

    Add data_loading.py file

---
+++ data_loading.py
@@ -0,0 +1,28 @@
+import json
+from pprint import pprint
+
+
+def load_data(filepath: str):
+    with open(filepath, 'r') as file:
+        json_order_data = json.load(file)
+    return json_order_data
+
+
+def check_consistent_keys(json_data: list) -> None:
+    # helper function to ensure the list of json dicts is consistent for when writing
+    # to a dataframe
+    reference_keys = set(json_data[0].keys())
+
+    for i, item in enumerate(json_data):
+        if not isinstance(item, dict):
+            print(f"Item at index {i} is not a dict.")
+        if set(item.keys()) != reference_keys:
+            print(f"Inconsistent keys found at index: {i}: {set(item.keys())}")
+
+
+if __name__ == "__main__":
+    data_dir = r'/Users/philip.papasavvas/Downloads/vmj_int/'
+    json_order_data = load_data(filepath=f"{data_dir}/task_data.json")
+    check_consistent_keys(json_data=json_order_data)
+    print("*** Preview of first item... ***")
+    pprint(json_order_data[0])
